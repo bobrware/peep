dc5791e019924c1dffbaef7647a0ab08ce390b61
Matthew Hrehirchuk <me@matthew-hre.com>
2026-06-14 15:15:31 -0600

    chore: scaffold initial project (#2)

---
+++ src/core/types.ts
@@ -0,0 +1,3 @@
+import type { Finding } from "./schema.js";
+
+export type ReviewResult<TFinding = Finding> = TFinding[];
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { z } from "zod";
+export { findingSchema } from "./core/schema.js";
+export type { Finding } from "./core/schema.js";
+export type { ReviewResult } from "./core/types.js";
